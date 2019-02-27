9a01a15b9c21996b244360428822c1e08f600c95
Dmitry <Dm.Matveev1996@yandex.ru>
2019-02-27 17:31:02 +0300

    27.02.2019

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from pywinauto import Application
+import time
 
 app = Application(backend='uia').connect(path="C:\\Users\\Dmitry\\AppData\\Local\\VtopeBot\\vtopebot.exe")
+app = app.Pane
 
-#b = app.Pane.child_window(title="Добавить аккаунт", control_type="Button").click()
-s = app.Pane.print_control_identifiers(depth=100)
+button_add_account = app.child_window(title="Добавить аккаунт", control_type="Button")
+button_add_account.click()
 
-s =2
+w = app.child_window(title="backgroundModalWidget", control_type="Custom")
+w.wait('exists')
+
+#s = app.print_control_identifiers(depth=100)
+
+app['Edit2'].set_text('123')
+app['Edit1'].set_text('1234')
+
+app['Добавить аккаунт2'].click()
+
+#w.wait_not('exists')
+
