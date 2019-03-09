d8747893fec0ecf405f75876ac414149cff19f3d
Dmitry <Dm.Matveev1996@yandex.ru>
2019-03-09 10:16:54 +0300

    09.03.2019

--- vserver/workers.py
+++ vserver/workers.py
@@ -5,6 +5,15 @@ from peewee import DoesNotExist
 from vserver.worker import Worker
 
 
+class Workers:
+    def __init__(self):
+        self.workers = None
+
+    async def init(self):
+        try:
+            self.workers = list(Worker.select())
+        except DoesNotExist:
+            self.workers = []
 
 
 
